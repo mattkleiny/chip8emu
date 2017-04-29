fb5caaa5e9b3302450f602f6db108ccb38558f7f
Matt Kleinschafer <xeusalmighty@gmail.com>
2017-04-29 12:29:26 +1000

    Simplify system layout and start writing decode function.

--- chip8/emulator.go
+++ chip8/emulator.go
@@ -2,42 +2,97 @@ package chip8
 
 type Opcode uint16 // 35 possible opcodes, which are all two bytes long
 
-// The combined chip 8 system
-type System struct {
-	CPU      CPU
-	Memory   Memory
-	Keypad   Keypad
-	Graphics Graphics
-	Audio    Audio
-}
-
 // The central processing unit of the chip 8 system
 type CPU struct {
+	Memory    [4096]byte // The Chip 8 has fixed 4K memory in total
 	Registers [16]byte   // 15 8-bit general purpose registers (V0, V1 through to VE). The 16th is the carry flag
 	I, PC     uint16     // An index register I, and a program counter PC which can have values from 0x000 to 0xFFF
 	Stack     [16]uint16 // The currently executing instruction; references the program counter
 	SP        uint16     // The stack pointer
-}
 
-// The memory bank of the chip 8 system
-type Memory struct {
-	Memory  [4096]byte // The Chip 8 has fixed 4K memory in total
-	System  []byte     // The core system memory; a slice of the system memory over 0x000-0x1FF
-	Font    []byte     // The font set; A slice of the system memory over 0x050-0x0A0
-	Working []byte     // The working ROM and RAM; a slice of the system memory over 0x200-0xFFF
-}
+	KeyState [16]byte      // The state of each individual key (up/down)
+	Pixels   [64 * 32]byte // 64 * 32 pixels (2048 total pixels)
 
-// The keypad on the chip 8 system
-type Keypad struct {
-	KeyState [16]byte // The state of each individual key (up/down)
-}
-
-// The graphics array on the chip 8 system
-type Graphics struct {
-	Pixels [64 * 32]byte // 64 * 32 pixels (2048 total pixels)
-}
-
-// The audio controller on the chip 8 system
-type Audio struct {
 	DelayTimer, SoundTimer byte // When above zero, they count down to zero. Counting occurs at 60hz
+}
+
+// The font-set for the chip 8 system
+var FontSet = []byte{
+	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+	0x20, 0x60, 0x20, 0x20, 0x70, // 1
+	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+	0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+}
+
+// Re-initializes the CPU
+func (cpu *CPU) Initialize() {
+	cpu.PC = 0x200 // program counter starts at 0x200
+	cpu.I = 0      // reset index register
+	cpu.SP = 0     // reset stack pointer
+
+	// TODO: clear display
+	// TODO: clear stack
+	// TODO: clear registers
+	// TODO: clear memory
+
+	// load the font-set
+	for i := 0; i < len(FontSet); i++ {
+		cpu.Memory[i] = FontSet[i]
+	}
+}
+
+// Loads a program into the system
+func (cpu *CPU) LoadProgram(program []byte) {
+	for i := 0; i < len(program); i++ {
+		cpu.Memory[i+0x200] = program[i] // programs are expected to start at 0x200
+	}
+}
+
+// Advances the system a single cycle
+func (cpu *CPU) NextCycle() {
+	// updates the CPU timers
+	updateTimers := func() {
+		if cpu.DelayTimer > 0 {
+			cpu.DelayTimer -= 1
+		}
+		if cpu.SoundTimer > 0 {
+			if cpu.SoundTimer == 1 {
+				println("BEEP")
+			}
+			cpu.SoundTimer -= 1
+		}
+	}
+
+	// fetch the next opcode based on the program counter
+	opcode := Opcode(cpu.Memory[cpu.PC]<<8 | cpu.Memory[cpu.PC+1])
+
+	// decode the opcode
+	switch opcode & 0xF000 {
+	case 0xA000: // ANNN: Sets I to the address NNN
+		cpu.I = uint16(opcode & 0x0FFF)
+		cpu.PC += 2
+
+	case 0x0000:
+		switch opcode & 0x000F {
+		default:
+			println("Unknown opcode: ", opcode)
+		}
+
+	default:
+		println("Unknown opcode: ", opcode)
+	}
+
+	updateTimers()
 }
--- main.go
+++ main.go
@@ -1,17 +1,45 @@
 package main
 
-import "github.com/veandco/go-sdl2/sdl"
+import (
+	"github.com/veandco/go-sdl2/sdl"
+	"github.com/xeusalmighty/chip8emu/chip8"
+)
 
+// Entry point for the emulator
 func main() {
+	system := new(chip8.CPU)
+	system.Initialize()
+
+	processor := func() {
+		system.NextCycle()
+	}
+
+	run(100, 100, 800, 600, processor)
+}
+
+// Bootstraps and executes the application via SDL
+func run(x, y, w, h int, frameHandler func()) {
 	sdl.Init(sdl.INIT_VIDEO | sdl.INIT_AUDIO)
 
-	window, err := sdl.CreateWindow("chip8emu", 100, 100, 800, 600, sdl.WINDOW_OPENGL)
+	// create a window with OpenGL
+	window, err := sdl.CreateWindow("chip8emu", x, y, w, h, sdl.WINDOW_OPENGL)
 	if err != nil {
 		panic("Failed to create main window")
 	}
 	defer window.Destroy()
 
-	// TODO: run the main loop
+	// runs the SDL event loop and calls the given callback function after event processing each cycle
+	running := true
+	for running == true {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				running = false
+			}
+		}
+		frameHandler() // execute the next frame
+		sdl.Delay(16)  // don't eat the cpu
+	}
 
 	sdl.Quit()
 }
